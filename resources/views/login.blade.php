59bb62e9e5620a089f55cbd184cadee246b8ae4d
saraswati21 <arunaacharya200@gmail.com>
2021-01-17 21:14:35 +0545

    login done with session

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,21 +5,22 @@
 <div class="container min-vh-100 pt-5">
     <div class="row">
         <div class="col-sm-6 mx-auto p-5">
-            <form>
+            <form  action="/login" method="post">
                 <div class="form-group">
+                    @csrf
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <div class="form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
                     <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
                 <br>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Login</button>
             </form>
         </div>
     </div>
